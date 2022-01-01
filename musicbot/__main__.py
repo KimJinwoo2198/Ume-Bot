49b314efb1a64e8617e0e8b9f95ee015643b2692
kimjw2198 <62290371+kimjw2198@users.noreply.github.com>
2022-01-01 12:13:07 +0900

    Add files via upload

---
+++ musicbot/__main__.py
@@ -0,0 +1,69 @@
+import time
+import json
+import discord
+import asyncio
+import requests
+import multiprocessing
+from urllib import request
+from discord.ext import commands
+
+from musicbot.lavalinkstart import child_process
+from musicbot import LOGGER, token, EXTENSIONS, commandInt, BOT_NAME_TAG_VER
+
+async def status_task():
+    while True:
+        try:
+            await bot.change_presence(
+                activity = discord.Game (f"{commandInt}help : 도움말"),
+                status = discord.Status.online,
+                afk = False
+            )
+            await asyncio.sleep(10)
+            await bot.change_presence(
+                activity = discord.Game ("%d개의 서버에서 놀고있어요!" %len(bot.guilds)),
+                status = discord.Status.online,
+                afk = False
+            )
+            await asyncio.sleep(10)
+        except Exception:
+            pass
+
+class Ume_Bot (commands.Bot) :
+    def __init__ (self) :
+        super().__init__ (
+            command_prefix=commandInt,
+            intents=intents
+        )
+        self.remove_command("help")
+        LOGGER.info("Lavalink Downloading...")
+        a = requests.get("https://api.github.com/repos/Cog-Creators/Lavalink-Jars/releases")
+        b = json.loads(a.text)
+        request.urlretrieve(f"https://github.com/Cog-Creators/Lavalink-Jars/releases/download/{b[0]['tag_name']}/Lavalink.jar", "Lavalink.jar")
+
+        process = multiprocessing.Process(target=child_process)
+        process.start()
+        time.sleep(20)
+
+        for i in EXTENSIONS :
+            self.load_extension ("musicbot.cogs." + i)
+
+    async def on_ready (self) :
+        LOGGER.info(BOT_NAME_TAG_VER)
+        await self.change_presence(
+            activity = discord.Game (f"{commandInt}help : 도움말"),
+            status = discord.Status.online,
+            afk = False
+        )
+        bot.loop.create_task(status_task())
+
+    async def on_message (self, message) :
+        if message.author.bot:
+            return
+        await self.process_commands (message)
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+
+bot = Ume_Bot()
+bot.run(token, bot=True)
---
+++ musicbot/config.py
@@ -0,0 +1,12 @@
+from musicbot.sample_config import Config
+
+class Development(Config):
+    TOKEN = ''
+    OWNERS = []
+    commandInt = ""
+    BOT_NAME = ""
+    BOT_TAG = ""
+    BOT_VER = ""
+    BOT_ID = 
+
+    psw = ""
